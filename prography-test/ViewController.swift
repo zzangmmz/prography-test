a3a0eb0917c3ef085294cfbe13b136c228eada79
zzangmmz <leemj036@gmail.com>
2025-02-12 22:46:27 +0900

    Initial Commit

---
+++ prography-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prography-test
+//
+//  Created by 이명지 on 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
