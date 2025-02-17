0bf58358239849f5bd54cc435e0cb448d593db18
zzangmmz <leemj036@gmail.com>
2025-02-18 00:11:36 +0900

    [feat] TMDB API 연동을 위해 TMDBAPI enum 생성, AccessToken 구조체 생성

---
+++ prography-test/Network/AccessToken.swift
@@ -0,0 +1,17 @@
+//
+//  AccessToken.swift
+//  prography-test
+//
+//  Created by 이명지 on 2/18/25.
+//
+
+import Foundation
+
+struct AccessToken {
+    static let accessToken: String = {
+        guard let accessToken = Bundle.main.infoDictionary?["AccessToken"] as? String else {
+            fatalError("Info.plist에서 Access Token을 읽어오지 못했습니다.")
+        }
+        return accessToken
+    }()
+}
