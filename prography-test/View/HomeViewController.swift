be1661321520935326f8d0df14aa90b6ca4ef07a
zzangmmz <leemj036@gmail.com>
2025-02-20 06:45:54 +0900

    [feat] 뷰모델 의존성 주입

--- prography-test/View/HomeViewController.swift
+++ prography-test/View/HomeViewController.swift
@@ -13,7 +13,17 @@ private enum Section: Int {
 }
 
 final class HomeViewController: UIViewController {
+    private var viewModel: HomeViewModel
     private var collectionView = UICollectionView()
+    
+    init(viewModel: HomeViewModel = HomeViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
