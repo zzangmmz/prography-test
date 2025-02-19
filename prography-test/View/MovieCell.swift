d4c3022493805472c37ef5cd594bdf8d8f56bd59
zzangmmz <leemj036@gmail.com>
2025-02-20 05:51:01 +0900

    [feat] cell 레이아웃 설정

--- prography-test/View/MovieCell.swift
+++ prography-test/View/MovieCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MovieCell: UICollectionViewCell {
     private let posterImageView: UIImageView = {
@@ -43,8 +44,19 @@ final class MovieCell: UICollectionViewCell {
         return stackView
     }()
     
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, overViewLabel, rateLabel, genreStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        setupSubviews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -54,13 +66,47 @@ final class MovieCell: UICollectionViewCell {
     private func setupSubviews() {
         [
             posterImageView,
-            titleLabel,
-            overViewLabel,
-            rateLabel,
-            genreStackView
+            contentStackView
         ].forEach {
-            addSubview($0)
+            contentView.addSubview($0)
         }
     }
-
+    
+    private func setupConstraints() {
+        posterImageView.snp.makeConstraints {
+            $0.height.equalToSuperview()
+            $0.width.equalTo(120)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.height.equalToSuperview()
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(16)
+            $0.trailing.equalToSuperview()
+        }
+    }
+    
+    func configure(with movie: Movie) {
+        self.posterImageView.kf.setImage(with: URL(string: movie.poster))
+        self.titleLabel.text = movie.title
+        self.overViewLabel.text = movie.overview
+        self.rateLabel.text = String(describing: movie.rate)
+        
+        self.genreStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        movie.genres.forEach {
+            let genreTagLabel = createGenreTagLabel(text: $0)
+            genreStackView.addArrangedSubview(genreTagLabel)
+        }
+    }
+    
+    private func createGenreTagLabel(text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = .systemFont(ofSize: 11, weight: .semibold)
+        label.textColor = .systemGray
+        label.layer.cornerRadius = 8
+        label.layer.borderWidth = 1
+        label.layer.borderColor = UIColor.onSurfaceVariant.cgColor
+        label.textAlignment = .center
+        return label
+    }
 }
