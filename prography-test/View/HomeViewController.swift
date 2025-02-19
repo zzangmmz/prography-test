f910ff9cf3c70b701203bf0fb97f63fd83c84166
zzangmmz <leemj036@gmail.com>
2025-02-19 23:21:27 +0900

    [feat] 네비게이션바 커스텀, 로고 이미지 등록

--- prography-test/View/HomeViewController.swift
+++ prography-test/View/HomeViewController.swift
@@ -11,5 +11,25 @@ final class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.shadowColor = .clear
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 144, height: 24))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "logo")
+        
+        let resizedImage = imageView.image?.withRenderingMode(.alwaysOriginal)
+        imageView.image = resizedImage
+        
+        navigationItem.titleView = imageView
     }
 }
