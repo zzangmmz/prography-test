2efefcca657a3bc8edf459dfd19f10b464e542e0
zzangmmz <leemj036@gmail.com>
2025-02-20 00:21:07 +0900

    [feat] 커스텀 캐러셀뷰 생성

---
+++ prography-test/View/CarouselView.swift
@@ -0,0 +1,50 @@
+//
+//  CarouselView.swift
+//  prography-test
+//
+//  Created by 이명지 on 2/19/25.
+//
+
+import UIKit
+import SnapKit
+
+final class CarouselView: UIView {
+    private var items: [Movie] = []
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.itemSize = CGSize(width: 316, height: 205)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPrefetchingEnabled = false
+        // cell register
+        
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func configure(with items: [Movie]) {
+        self.items = items
+        collectionView.reloadData()
+    }
+}
