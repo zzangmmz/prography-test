5feb906a9cd605fc621c7434e307d39291184958
zzangmmz <leemj036@gmail.com>
2025-02-20 12:17:44 +0900

    [feat] 받아온 영화 릴레이에 저장

--- prography-test/ViewModel/HomeViewModel.swift
+++ prography-test/ViewModel/HomeViewModel.swift
@@ -5,12 +5,17 @@
 //  Created by 이명지 on 2/20/25.
 //
 
+import RxSwift
+import RxCocoa
+
 final class HomeViewModel {
+    private var disposeBag = DisposeBag()
     private let movieRepository: MovieRepositoryProtocol
     
-    private(set) var nowPlayingMovies = [Movie]()
-    private(set) var popularMovies = [Movie]()
-    private(set) var topRatedMovies = [Movie]()
+    private(set) var nowPlayingMovies = BehaviorRelay<[Movie]>(value: [])
+    private(set) var popularMovies = BehaviorRelay<[Movie]>(value: [])
+    private(set) var topRatedMovies = BehaviorRelay<[Movie]>(value: [])
+    private(set) var selectedCategory = BehaviorRelay<MovieCategory>(value: .nowPlaying)
     
     init(movieRepository: MovieRepositoryProtocol = MovieRepository()) {
         self.movieRepository = movieRepository
