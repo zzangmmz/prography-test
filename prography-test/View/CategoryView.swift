aa3056e5d34f314647bc3cfa21db3dcb696f9488
zzangmmz <leemj036@gmail.com>
2025-02-20 09:31:16 +0900

    [refactor] 컴포지셔널 레이아웃 삭제, RxSwift 패키지 삭제

---
+++ prography-test/View/CategoryView.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryView.swift
+//  prography-test
+//
+//  Created by 이명지 on 2/20/25.
+//
+
+import UIKit
+
+final class CategoryView: UICollectionView {
+    private var categories = MovieCategory.allCases
+    
+    private(set) lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPrefetchingEnabled = false
+        collectionView.register(CarouselViewCell.self, forCellWithReuseIdentifier: String(describing: CarouselViewCell.self))
+        
+        return collectionView
+    }()
+}
