1ddbe412c575741b1b46ae5c3d9ed40aadbe9ce8
zzangmmz <leemj036@gmail.com>
2025-02-21 17:47:46 +0900

    [feat] 영화 id, review 프로퍼티 정의, 생성자 정의

--- prography-test/View/ViewController/ReviewViewController.swift
+++ prography-test/View/ViewController/ReviewViewController.swift
@@ -6,8 +6,22 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class ReviewViewController: UIViewController {
+    private var disposeBag = DisposeBag()
+    private var movieID: Int
+    private var review: Review?
+    
+    init(movieID: Int) {
+        self.movieID = movieID
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
